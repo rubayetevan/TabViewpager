534f118008f32cb4f191ff079a8161d691d1b47f
Rubayet <rubayet.evan@gmail.com>
2016-05-30 13:43:48 +0600

    Initial commit

---
+++ app/src/main/java/com/bdjobs/tabviewpager/ThreeFragment.java
@@ -0,0 +1,21 @@
+package com.bdjobs.tabviewpager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rubayet on 30-May-16.
+ */
+public class ThreeFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_three,container,false);
+        return view;
+    }
+}
